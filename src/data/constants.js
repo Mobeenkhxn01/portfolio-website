bd7c50bb985502247a5a6d790270613a6bba14aa
Mobeenkhxn01 <mobeenkhan191915@gmail.com>
2025-08-31 02:38:47 +0530

    complete portfolio-website

---
+++ src/data/constants.js
@@ -0,0 +1,316 @@
+export const Bio = {
+  name: "Mobeen Khan",
+  roles: [
+    "Software Developer",
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "A passionate full-stack developer turning ideas into scalable, user-centric applications with clean code and modern tech.",
+  github: "https://github.com/Mobeenkhxn01",
+  resume: "https://www.linkedin.com/in/Mobeenkhan456/",
+  linkedin: "https://www.linkedin.com/in/Mobeenkhan456/",
+  twitter: "https://x.com/errornotfount33",
+  insta: "https://www.instagram.com/mobeenkhxn01/",
+  facebook: "https://www.linkedin.com/in/mobeenkhxn01/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind css",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNhoXisDruJMDAq3Ltd-wuaMW2lGxck9wAKw&s",
+      },
+      {
+        name:"TypeScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://logowik.com/content/uploads/images/express-js1720895497.logowik.com.webp",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThmT8HtltidnDUJvGcRYzg8B9h8zM-2O-FZw&s",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png",
+      },
+      {
+        name:"Bun",
+        image: "https://ih1.redbubble.net/image.5227224545.3198/st,small,507x507-pad,600x600,f8f8f8.jpg"
+      },
+      {
+        name:"Prisma ORM",
+        image: "https://website-prisma.vercel.app/og-images/og-image-about.png"
+      },
+    ],
+  },
+
+  {
+    title: "Programming Fundaments",
+    skills: [
+      {
+        name: "C/C++",
+        image:
+          "https://raw.githubusercontent.com/bablubambal/All_logo_and_pictures/1ac69ce5fbc389725f16f989fa53c62d6e1b4883/programming%20languages/c++.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+       },
+      
+      {
+        name: "Java",
+        image:
+          "https://www.citypng.com/public/uploads/preview/java-white-logo-image-png-701751694771838qyus1n9gdu.png",
+      },
+      
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://miro.medium.com/v2/0*JLTX-AJQ0oC40mjc.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://ik.imagekit.io/qualys/wp-content/uploads/2020/10/postman.jpg",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://thumbnail.imgbin.com/12/16/12/imgbin-arch-linux-computer-icons-theme-kde-linux-white-and-blue-logo-dDuYVnjgWAaY4UYc9rkUXcZfP_t.jpg",
+      },
+      {
+        name: "Render",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfEDq_cV2AytxA_--IMzA0x8SwvJ94Q9SvRA&s",
+      },
+      {
+        name:"Vercel",
+        image:
+          "https://images.seeklogo.com/logo-png/39/1/vercel-logo-png_seeklogo-396226.png"
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://raw.githubusercontent.com/Mobeenkhxn01/certificates/main/DevCreate%20Hackathon.png",
+    role: "DevCreate Hackathon  Runner Up",
+    company: "Google Developer Club, Jalandhar",
+    date: "Mar 2024",
+    desc: `Secured 2nd place in a 24-hour hackathon organized by the Google Developer Club. Led a team of 4 in designing and developing a "Project Management Website" based on hierarcy model. Contributed to frontend architecture, task delegation, and final pitch presentation. Gained valuable experience in collaboration, rapid prototyping, and problem-solving under pressure.`,
+    skills: [
+      "Hackathon Winning",
+      "Idea Brainstorming",
+      "Team Spirit",
+    ],
+    doc: "https://raw.githubusercontent.com/Mobeenkhxn01/certificates/main/DevCreate%20Hackathon.png",
+  },
+   {
+    id: 1,
+    img: "https://raw.githubusercontent.com/Mobeenkhxn01/certificates/main/cloudIoT.png",
+    role: "Foundation of IoT Cloud",
+    company: "Nptel Swayam",
+    date: "Oct 2024 - Present",
+    desc: "Catalyst for Student-driven tech events for diverse technologies, fostering collaboration and problem-solving in a dynamic environment",
+    skills: [
+      "Internet of Things",
+      "Edge Computing",
+      "Cloud Computing",
+      "AWS"
+      
+    ],
+    doc: "https://raw.githubusercontent.com/Mobeenkhxn01/certificates/main/cloudIoT.png",
+  },
+  {
+    id: 1,
+    img: "https://raw.githubusercontent.com/Mobeenkhxn01/certificates/main/code.jpeg",
+    role: "Co-Tech Head",
+    company: "C-Square Club",
+    date: "Oct 2024 - Present",
+    desc: "Catalyst for Student-driven tech events for diverse technologies, fostering collaboration and problem-solving in a dynamic environment",
+    skills: [
+      "Competitive Programming",
+      "DSA",
+      "Team Work",
+      "C++",
+      "Co-ordinator",
+    ],
+    doc: "https://raw.githubusercontent.com/Mobeenkhxn01/certificates/main/code.jpeg",
+  },
+
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor",
+    company: "CoDE",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    skills: [
+      "Working in Tech Community",
+      "Problem Solving",
+      "Excellent Communication",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+  },
+  
+  
+];
+
+ export const education = [
+    {
+      id: 0,
+      img: "https://images.seeklogo.com/logo-png/43/1/chandigarh-university-cu-logo-png_seeklogo-432515.png",
+      school: "Chandigarh University, India",
+      date: "Aug 2022 - Jun 2026",
+      grade: "7.45 CGPA",
+      desc: "Currently pursuing final year of B.Tech with a focus on software development, web technologies, and data structures. Gained hands-on experience through academic projects, coding assignments, and lab work. Strengthening skills in both theoretical concepts and real-world application development.",
+      degree: "Bachelor of Engineering - BE,  Computer Science & Engineering",
+    },
+    {
+      id: 1,
+      img: "https://bingo.icbse.com/v1/business.jpg?action=logo&id=3eenz",
+      school: "Guru Nanak Dev Public School, Bilaspur, Rampur",
+      date: "2021",
+      grade: "87.6%",
+      desc: "I completed my class 12 College at GND Public School, Bisharat Nagar Bilaspur 244921, where I studied Science with Physic, Chemistry and Mathematics as core subjects ",
+      degree: "HSC(XII), Science",
+    },
+    {
+      id: 2,
+      img: "https://bingo.icbse.com/v1/business.jpg?action=logo&id=3eenz",
+      school: "Guru Nanak Dev Public School, Bilaspur, Rampur",
+      date: "2018- 2019",
+      grade: "87%",
+      desc: "I completed my class 12 College at GND Public School, Bisharat Nagar Bilaspur 244921 ",
+      degree: "SSC(X), Science",
+    },
+  ];
+
+
+export const projects = [
+  {
+    id: 1,
+    title: "Qrave-bites",
+    date: "Aug 2025",
+    description:
+     "Qrave Bites is an end-to-end digital dining solution that transforms how restaurants serve and how customers order, offering a seamless experience similar to a modern dine-in QR code ordering platform. With features for table-based QR code menus, instant ordering, secure online payments, and real-time order tracking, Qrave Bites empowers restaurants to streamline operations while providing customers with a fast, contactless, and convenient dining experience.",
+    image:
+      "https://raw.githubusercontent.com/Mobeenkhxn01/certificates/main/qrave-bite-image.jpg",
+    tags: [
+      "Next Js",
+      "Tailwind Css",
+      "Prisma Orm",
+      "Tanstack Query",
+      "AWS S3",
+      "MongoDB",
+      "Render",
+      "Node Js",
+      
+    ],
+    category: "web app",
+    githubLink: "https://github.com/Mobeenkhxn01/qrave-bites",
+    github: "https://github.com/Mobeenkhxn01/qrave-bites",
+    webapp: "https://github.com/Mobeenkhxn01/qrave-bites",
+  },
+  {
+  id: 2,
+  title: "Todos App",
+  date: "Aug 2025",
+  description:
+    "A full-stack Todo application that allows users to manage daily tasks with authentication, real-time updates, and a clean UI. The app features task creation, completion toggling, and deletion with persistent storage using Prisma ORM and secure authentication via NextAuth. Designed with TanStack Query for data synchronization and React for interactive UI, it provides a smooth and efficient task management experience.",
+  image:
+    "https://raw.githubusercontent.com/Mobeenkhxn01/certificates/main/todos-app-image.jpg",
+  tags: [
+    "Next Js",
+    "TypeScript",
+    "Tailwind Css",
+    "Prisma Orm",
+    "Tanstack Query",
+    "MongoDB",
+    "NextAuth",
+    "ShadCN UI",
+  ],
+  category: "web app",
+  githubLink: "https://github.com/Mobeenkhxn01/todos-app",
+  github: "https://github.com/Mobeenkhxn01/todos-app",
+  webapp:"https://todos-app-lzs2.vercel.app/",
+},
+
+];
+
+
+
+          
+
